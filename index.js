9a5a4c8d3e31776192f8628a69b9a07be67888a4
BrandonMager <sotogarciabrandon@gmail.com>
2024-07-26 21:06:38 -0700

    Boilerplate

--- index.js
+++ index.js
@@ -2,3 +2,22 @@ const express = require("express")
 const { MongoClient } = require("mongodb")
 
 const app = express()
+const cors = require("cors")
+const dotenv = require('dotenv')
+
+dotenv.config()
+
+const port = process.env.PORT || 3000
+app.use(cors())
+app.use(express.json())
+
+const client = new MongoClient(proces.env.MONGO_URI)
+
+client.connect(err => {
+    if(err) {
+        console.error("Unable to connect to the MongoDB URI: ", err)
+        process.exit(1)
+    }
+
+})
+
