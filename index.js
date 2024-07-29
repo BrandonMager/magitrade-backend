c73058ca5ed94e0c0d02424160f65190bd4e0c8a
BrandonMager <sotogarciabrandon@gmail.com>
2024-07-28 19:58:48 -0700

    MongoDB connection

--- index.js
+++ index.js
@@ -7,17 +7,37 @@ const dotenv = require('dotenv')
 
 dotenv.config()
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5100
 app.use(cors())
 app.use(express.json())
 
-const client = new MongoClient(proces.env.MONGO_URI)
+const client = new MongoClient(process.env.MONGO_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true
+})
 
 client.connect(err => {
     if(err) {
         console.error("Unable to connect to the MongoDB URI: ", err)
         process.exit(1)
     }
+})
 
+const db = client.db("findata")
+
+app.get('/user/:userId', async (req, res) => {
+    const { userId} = req.params
+    const item =  await db.collection('Users').findOne({
+        userId: userId
+    })
+
+    if(!item) {
+        return res.status(404).send("Could not find user")
+    }
+
+    return res.json(item)
+})
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
 })
 
