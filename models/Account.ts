9235937445f597d710fda19a8196ab582dba0d3b
BrandonMager <sotogarciabrandon@gmail.com>
2024-08-03 16:00:47 -0700

    Added Account Model

---
+++ models/Account.ts
@@ -0,0 +1,16 @@
+import { Document, model, Schema } from "mongoose";
+
+interface AccountInteface extends Document{
+    userId: string,
+    email: string
+    password: string,
+}
+
+const AccountSchema = new Schema<AccountInteface>({
+    userId: {type: String, required: true},
+    email: {type: String, required: true},
+    password: {type: String, required: true},
+})
+
+const accountModel = model<AccountInteface>('Account', AccountSchema, "Accounts")
+export default accountModel
